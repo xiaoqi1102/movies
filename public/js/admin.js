683f6bfdf0ba54f9968587f2498e624c43e73122
小齐 <705764065@qq.com>
2016-08-18 18:23:10 +0800

    绑定删除函数和请求逻辑

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,21 @@
 /**
  * Created by xiaoqi on 16/8/15.
  */
+$(function () {
+    $('.del').bind('click',function (e) {
+        var target=$(e.target);
+        var id =target.data('id');
+        var tr=$('.item-id-'+id);
+        $.ajax({
+            type:'delete',
+            url:'/admin/list?id='+id
+        })
+            .done(function (results) {
+                if(results.success===1){
+                    if(tr.length>0){
+                        tr.remove()
+                    }
+                }
+            })
+    })
+});
